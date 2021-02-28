094f20a3e6bbe7000b6c930b5165a29b0bc7955d
jh2737 <jh2737@cornell.edu>
2021-02-28 14:29:37 -0500

    bash file for question 1

--- hw2_part_1.sh
+++ hw2_part_1.sh
@@ -1,1 +1,15 @@
+# Step 1 create readme file and add text
+touch README.md
+# Step 2 create gitignore file and add text
 touch .gitignore
+# Step 3 create new-branch
+git checkout -b new-branch
+# Step 4 edit README file
+# Step 5 add, commmit and push the README file (in new-branch)
+git add README.md
+git commit -m "Added Goodbye World to readme file"
+git push -u origin new-branch
+# Step 6 change branch to main and merge with new-branch
+git checkout main
+git merge new-branch
+git push -u origin main
