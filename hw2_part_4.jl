a957df6c0f2c9cc7ccb33e0adbc991404946e064
jh2737 <jh2737@cornell.edu>
2021-03-17 12:05:16 -0400

    final coding file for part 4

--- hw2_part_4.jl
+++ hw2_part_4.jl
@@ -1,31 +1,33 @@
 
 # Utility function without constraint
-using Optim, ForwardDiff
-function utility_maximizer(kappa, eta, p1, p2, w, c1, solver)
-    e1 = (eta-1)/eta
-    e2 = eta/(eta-1)
-    f(c1) = kappa*c1^e1+(1-kappa)*((w-p1*c1)/p2)^e1
-    u(c1) = -f(c1)^e2
-    # first derivative function
-    # u'(c1) = -e2 * f(c1)^(e2-1) * (e1*kappa*c1^(e1-1) + e1*(1-kappa)*((w-p1*c1)/p2)^(e1-1))
+using Optim
 
-    function u!(storage, c1)
-        storage[1] = -e2 * f(c1)^(e2-1) * (e1*kappa*c1^(e1-1) + e1*(1-kappa)*((w-p1*c1)/p2)^(e1-1))
-    end
+kappa = 0.5
+eta = 2.0
+p1 = 1.0
+p2 = 1.0
+w = 10.0
+e1 = (eta-1)/eta
+e2 = eta/(eta-1)
+initial_guess = [1.0]
 
-    # second derivative function
-    # second derivative
-    # set h(c1) = e1*kappa*c1^(e1-1) + e1*(1-kappa)*((w-p1*c1)/p2)^(e1-1)
-    # u''(c1) = -e2*(e2-1)*f(c1)^(e2-2)*h(c1)*h(c1) - e2*f(c1)^(e2-1)*(e1*(e1-1)*kappa*c1^(e1-2)+e1*(e1-1)*(1-kappa)*(w-p1*c1)/p2)^(e1-2))
-    h(c1) = e1*kappa*c1^(e1-1) + e1*(1-kappa)*((w-p1*c1)/p2)^(e1-1)
-    function uu!(storage, c1)
-        storage[1] = -e2*(e2-1)*f(c1)^(e2-2)*h(c1)*h(c1) -
-        e2*f(c1)^(e2-1)*(e1*(e1-1)*kappa*c1^(e1-2)+e1*(e1-1)*(1-kappa)*(w-p1*c1)/p2)^(e1-2))
-    end
-    if solver == "Newton"
-        output = optimize(u, u!, uu!, 0)
-    elseif solver == "Conjugate gradient"
-        output = optimize(c1->f(first(c1)),[initial_guess], ConjugateGradient())
+function f_first!(storage, c1)
+    storage[1] = -1*e2*
+    ((kappa*c1[1]^(e1)+(1-kappa)*((w-p1*c1[1])/p2)^(e1))^(e2-1))*
+    (e1*kappa*(c1[1])^((e1)-1)-e1*(1-kappa)*((w-p1*c1[1])/p2)^(e1-1)*(p1/p2))
+end
+
+
+function utility_maximizer(kappa, eta, p1, p2, w, initial_guess, solver)
+
+    f(c1) = -1*(kappa*c1[1]^e1+(1-kappa)*((w-p1*c1[1])/p2)^e1)^e2
+    lower = [1e-3]
+    upper = [w/p1]
+
+    if solver == "Conjugate gradient"
+        output = Optim.minimizer(optimize(f, f_first!, lower, upper, initial_guess, Fminbox(ConjugateGradient())))
     end
     return output
 end
+
+utility_maximizer(kappa, eta, p1, p2, w, initial_guess, "Conjugate gradient")
